7755a8ffce2ed1f3805242ac676fb4f83f2768a8
Gary Johnson <gary@gjtt.com>
2017-05-09 22:32:33 -0400

    Make gcodepainter more resilient to printer state

--- gcodepainter/gcodepainter/services/gcode_sender.py
+++ gcodepainter/gcodepainter/services/gcode_sender.py
@@ -4,8 +4,8 @@ import serial
 import threading
 from queue import Queue
 
-#PORT='/dev/ttys001'
-PORT='/dev/tty.usbmodem1461'
+PORT='/dev/ttys009'
+#PORT='/dev/tty.usbmodem1461'
 SPEED=4800.0
 
 class GcodeSender(object):
@@ -45,54 +45,27 @@ class GcodeSender(object):
         self.command_queue.put_nowait('G90')
         self.plotter = serial.Serial(PORT, 115200, timeout=1)
 
-        while True:
-            if not self._read_line().startswith('wait'):
-                continue
+        self._read_and_process_and_wait_for_ok(break_on_timeout=True)
 
+        while True:
             while not self.command_queue.empty():
                 command = self.command_queue.get_nowait()
                 self.command_queue.task_done()
-
                 self._send_line(command)
-                response = self._read_line()
-
-                while not response.startswith('ok'):
-                    if response.startswith('rs {}'.format(self.line_number-1)):
-                        print('resend request: {}'.format(response))
-                        self.line_number = self.line_number-1
-                        self._send_line(command)
-                        response = self._read_line()
-                    elif response.startswith('Resend:{}'.format(self.line_number-1)):
-                        print('resend request: {}'.format(response))
-                        self.line_number = self.line_number-1
-                        self._send_line(command)
-                        response = self._read_line()
-                    elif response.startswith('rs') or response.startswith('Resend'):
-                        raise Exception('requested resend of some other line number: {}'.format(response))
-                    elif response.startswith('!!'):
-                        raise Exception('printer fault')
-                    elif response.startswith('//'):
-                        print('comment: {}'.format(response))
-                        response = self._read_line()
-                    elif response.startswith('wait'):
-                        break
-                    else:
-                        print('unknown response: {}'.format(response))
-                        response = self._read_line()
-                        #raise Exception('unknown response: {}'.format(response))
+                self._read_and_process_and_wait_for_ok()
 
             time.sleep(0.5)
 
     def _send_line(self, line):
         command = 'N{} {} '.format(self.line_number, line)
         command = '{}*{}\n'.format(command, self._checksum(command))
-        print(command)
+        print("SEND: {}".format(command))
         self.line_number += 1
         self.plotter.write(command.encode('utf-8'))
         
     def _read_line(self):
         response = self.plotter.readline()
-        print(response)
+        print("READ: {}".format(response))
         return response.decode('utf-8')
 
     def _checksum(self, command):
@@ -102,6 +75,36 @@ class GcodeSender(object):
             int_char = int.from_bytes(byte_char, 'big')
             checksum  = checksum ^ int_char
         return checksum
+
+    def _read_and_process_and_wait_for_ok(self, break_on_timeout=False):
+        response = self._read_line()
+
+        if not response.strip() and break_on_timeout:
+            return
+
+        previous_line_number = self.line_number-1
+        while not response.startswith('ok'):
+            if response.startswith((f"rs {previous_line_number}", f"Resend:{previous_line_number}")):
+                print('resend request: {}'.format(response))
+                self.line_number = self.line_number-1
+                self._send_line(command)
+                response = self._read_line()
+            elif response.startswith(('rs', 'Resend')):
+                raise Exception('requested resend of some other line number: {}'.format(response))
+            elif response.startswith('!!'):
+                raise Exception('printer fault')
+            elif response.startswith('//'):
+                print('comment: {}'.format(response))
+                response = self._read_line()
+            elif response.startswith('wait'):
+                response = self._read_line()
+                time.sleep(0.5)
+            elif response.startswith('start'):
+                return
+            else:
+                print('unknown response: {}'.format(response))
+                response = self._read_line()
+                #raise Exception('unknown response: {}'.format(response))
 
     def stop_thread(self):
         self._stop.set()
